41f0f71def7983b077c3ea78d36203b3d2fcfa17
gcanti <giulio.canti@gmail.com>
2017-07-21 12:10:24 +0200

    upgrade to latest fp-ts (0.4.1)

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,19 +1,14 @@
 const path = require('path')
 
 module.exports = {
-	entry: "./example/index.ts",
-	output: {
-		filename: "./example/bundle.js"
-	},
-	resolve: {
-		extensions: ["", ".js", ".ts", ".tsx"],
-		root: [
-			path.resolve(__dirname, 'src/app')
-		]
-	},
-	module: {
-		loaders: [
-			{ test: /\.tsx?$|\.jsx?$/, loader: "awesome-typescript-loader", exclude: /node_modules/ }
-		]
-	}
-};
+  entry: './example/index.ts',
+  output: {
+    filename: './example/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  module: {
+    loaders: [{ test: /\.tsx?$|\.jsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ }]
+  }
+}
