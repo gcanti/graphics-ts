8dd936c02514224bdcf66305d923471666ea3d71
Maxwell Brown <maxwellbrown1990@gmail.com>
2020-07-16 12:22:58 -0400

    Update tags in Shape module

--- src/Shape.ts
+++ src/Shape.ts
@@ -8,23 +8,14 @@ import { Foldable, Foldable1, Foldable2, Foldable3 } from 'fp-ts/lib/Foldable'
 import * as RA from 'fp-ts/lib/ReadonlyArray'
 import * as M from 'fp-ts/lib/Monoid'
 
-/**
- * Represents a shape that can be drawn.
- *
- * @since 1.0.0
- */
-export type Shape = Arc | Composite | Ellipse | Path | Rect
-
-/**
- * Represents an angle specified in either degrees or radians.
- *
- * @since 1.0.0
- */
-export type Angle = Degrees | Radians
+// -------------------------------------------------------------------------------------
+// model
+// -------------------------------------------------------------------------------------
 
 /**
  * Represents an angle specified in degrees.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Degrees {
@@ -39,6 +30,7 @@ export interface Degrees {
 /**
  * Represents an angle specified in radians.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Radians {
@@ -51,37 +43,17 @@ export interface Radians {
 }
 
 /**
- * Constructs an angle specified in degrees.
+ * Represents an angle specified in either degrees or radians.
  *
+ * @category model
  * @since 1.0.0
  */
-export const degrees = (degrees: number): Degrees => ({ _tag: 'Degrees', degrees })
-
-/**
- * Constructs an angle specified in degrees.
- *
- * @since 1.0.0
- */
-export const radians = (radians: number): Radians => ({ _tag: 'Radians', radians })
-
-/**
- * Converts an angle into radians for use with the canvas.
- *
- * @since 1.0.0
- */
-export const angle = (angle: Angle): number => {
-  switch (angle._tag) {
-    case 'Degrees':
-      return angle.degrees * (Math.PI / 180)
-
-    case 'Radians':
-      return angle.radians
-  }
-}
+export type Angle = Degrees | Radians
 
 /**
  * A single point consisting of `x` and `y` coordinates on a two-dimensional plane.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Point {
@@ -97,16 +69,10 @@ export interface Point {
 }
 
 /**
- * Constructs a `Point` from x and y coordinates.
- *
- * @since 1.0.0
- */
-export const point = (x: number, y: number): Point => ({ x, y })
-
-/**
  * An arc with center coordinates `x` and `y`, radius `r`, starting and ending angles `start` and `end`,
  * and travels in the direction given by `anticlockwise` (defaulting to clockwise)
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Arc {
@@ -144,45 +110,9 @@ export interface Arc {
 }
 
 /**
- * Constructs an `Arc` shape.
- *
- * @since 1.0.0
- */
-export const arc = (
-  x: number,
-  y: number,
-  r: number,
-  start: Angle,
-  end: Angle,
-  anticlockwise: boolean = false
-): Arc => ({
-  _tag: 'Arc',
-  x,
-  y,
-  r,
-  start: angle(start),
-  end: angle(end),
-  anticlockwise
-})
-
-/**
- * Constructs an `Arc` that forms a circle shape.
- *
- * @since 1.0.0
- */
-export const circle = (x: number, y: number, r: number): Arc => ({
-  _tag: 'Arc',
-  x,
-  y,
-  r,
-  start: angle(radians(0)),
-  end: angle(radians(Math.PI * 2)),
-  anticlockwise: false
-})
-
-/**
  * Represents a shape that is composed of several other shapes.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Composite {
@@ -195,20 +125,11 @@ export interface Composite {
 }
 
 /**
- * Constructs a `Composite` shape.
- *
- * @since 1.0.0
- */
-export const composite = (shapes: ReadonlyArray<Shape>): Composite => ({
-  _tag: 'Composite',
-  shapes
-})
-
-/**
  * An elliptical arc centered at (`x`, `y`) with the radii radiusX and radiusY specified by `rx` and `ry`.
  * The path starts at startAngle and ends at endAngle, specified by `start` and `end`, with the specified
  * `rotation` and travels in the direction given by `anticlockwise` (defaulting to clockwise).
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Ellipse {
@@ -256,34 +177,9 @@ export interface Ellipse {
 }
 
 /**
- * Constructs an `Ellipse` shape.
- *
- * @since 1.0.0
- */
-export const ellipse = (
-  x: number,
-  y: number,
-  rx: number,
-  ry: number,
-  rotation: Angle,
-  start: Angle,
-  end: Angle,
-  anticlockwise: boolean = false
-): Ellipse => ({
-  _tag: 'Ellipse',
-  x,
-  y,
-  rx,
-  ry,
-  rotation: angle(rotation),
-  start: angle(start),
-  end: angle(end),
-  anticlockwise
-})
-
-/**
  * A path is a list of points joined by line segments.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Path {
@@ -301,55 +197,9 @@ export interface Path {
 }
 
 /**
- * The `Monoid` instance for a `Path`.
- *
- * @since 1.0.0
- */
-export const monoidPath: M.Monoid<Path> = M.getStructMonoid({
-  _tag: { concat: () => 'Path', empty: 'Path' },
-  closed: M.monoidAny,
-  points: RA.getMonoid<Point>()
-})
-
-/**
- * Constructs a closed `Path` shape from a `Foldable` of `Point`s.
- *
- * @since 1.0.0
- */
-export function closed<F extends URIS3>(foldable: Foldable3<F>): <E, A>(fa: Kind3<F, E, A, Point>) => Path
-export function closed<F extends URIS2>(foldable: Foldable2<F>): <A>(fa: Kind2<F, A, Point>) => Path
-export function closed<F extends URIS>(foldable: Foldable1<F>): (fa: Kind<F, Point>) => Path
-export function closed<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path
-export function closed<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path {
-  return (fa) =>
-    F.reduce(fa, monoidPath.empty, (b, a) => ({
-      _tag: 'Path',
-      closed: true,
-      points: RA.snoc(b.points, a)
-    }))
-}
-
-/**
- * Constructs an open `Path` shape from a `Foldable` of `Point`s.
- *
- * @since 1.0.0
- */
-export function path<F extends URIS3>(foldable: Foldable3<F>): <E, A>(fa: Kind3<F, E, A, Point>) => Path
-export function path<F extends URIS2>(foldable: Foldable2<F>): <A>(fa: Kind2<F, A, Point>) => Path
-export function path<F extends URIS>(foldable: Foldable1<F>): (fa: Kind<F, Point>) => Path
-export function path<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path
-export function path<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path {
-  return (fa) =>
-    F.reduce(fa, monoidPath.empty, (b, a) => ({
-      _tag: 'Path',
-      closed: false,
-      points: RA.snoc(b.points, a)
-    }))
-}
-
-/**
  * Represents a rectangle with top-left corner coordinates at `x` and `y`.
  *
+ * @category model
  * @since 1.0.0
  */
 export interface Rect {
@@ -377,8 +227,176 @@ export interface Rect {
 }
 
 /**
+ * Represents a shape that can be drawn.
+ *
+ * @category model
+ * @since 1.0.0
+ */
+export type Shape = Arc | Composite | Ellipse | Path | Rect
+
+// -------------------------------------------------------------------------------------
+// constructors
+// -------------------------------------------------------------------------------------
+
+/**
+ * Constructs an angle specified in degrees.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const degrees = (degrees: number): Degrees => ({ _tag: 'Degrees', degrees })
+
+/**
+ * Constructs an angle specified in degrees.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const radians = (radians: number): Radians => ({ _tag: 'Radians', radians })
+
+/**
+ * Converts an angle into radians for use with the canvas.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const angle = (angle: Angle): number => {
+  switch (angle._tag) {
+    case 'Degrees':
+      return angle.degrees * (Math.PI / 180)
+
+    case 'Radians':
+      return angle.radians
+  }
+}
+
+/**
+ * Constructs a `Point` from x and y coordinates.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const point = (x: number, y: number): Point => ({ x, y })
+
+/**
+ * Constructs an `Arc` shape.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const arc = (
+  x: number,
+  y: number,
+  r: number,
+  start: Angle,
+  end: Angle,
+  anticlockwise: boolean = false
+): Arc => ({
+  _tag: 'Arc',
+  x,
+  y,
+  r,
+  start: angle(start),
+  end: angle(end),
+  anticlockwise
+})
+
+/**
+ * Constructs an `Arc` that forms a circle shape.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const circle = (x: number, y: number, r: number): Arc => ({
+  _tag: 'Arc',
+  x,
+  y,
+  r,
+  start: angle(radians(0)),
+  end: angle(radians(Math.PI * 2)),
+  anticlockwise: false
+})
+
+/**
+ * Constructs a `Composite` shape.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const composite = (shapes: ReadonlyArray<Shape>): Composite => ({
+  _tag: 'Composite',
+  shapes
+})
+
+/**
+ * Constructs an `Ellipse` shape.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export const ellipse = (
+  x: number,
+  y: number,
+  rx: number,
+  ry: number,
+  rotation: Angle,
+  start: Angle,
+  end: Angle,
+  anticlockwise: boolean = false
+): Ellipse => ({
+  _tag: 'Ellipse',
+  x,
+  y,
+  rx,
+  ry,
+  rotation: angle(rotation),
+  start: angle(start),
+  end: angle(end),
+  anticlockwise
+})
+
+/**
+ * Constructs a closed `Path` shape from a `Foldable` of `Point`s.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export function closed<F extends URIS3>(foldable: Foldable3<F>): <E, A>(fa: Kind3<F, E, A, Point>) => Path
+export function closed<F extends URIS2>(foldable: Foldable2<F>): <A>(fa: Kind2<F, A, Point>) => Path
+export function closed<F extends URIS>(foldable: Foldable1<F>): (fa: Kind<F, Point>) => Path
+export function closed<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path
+export function closed<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path {
+  return (fa) =>
+    F.reduce(fa, monoidPath.empty, (b, a) => ({
+      _tag: 'Path',
+      closed: true,
+      points: RA.snoc(b.points, a)
+    }))
+}
+
+/**
+ * Constructs an open `Path` shape from a `Foldable` of `Point`s.
+ *
+ * @category constructors
+ * @since 1.0.0
+ */
+export function path<F extends URIS3>(foldable: Foldable3<F>): <E, A>(fa: Kind3<F, E, A, Point>) => Path
+export function path<F extends URIS2>(foldable: Foldable2<F>): <A>(fa: Kind2<F, A, Point>) => Path
+export function path<F extends URIS>(foldable: Foldable1<F>): (fa: Kind<F, Point>) => Path
+export function path<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path
+export function path<F>(F: Foldable<F>): (fa: HKT<F, Point>) => Path {
+  return (fa) =>
+    F.reduce(fa, monoidPath.empty, (b, a) => ({
+      _tag: 'Path',
+      closed: false,
+      points: RA.snoc(b.points, a)
+    }))
+}
+
+/**
  * Constructs a `Rectangle` shape.
  *
+ * @category constructors
  * @since 1.0.0
  */
 export const rect = (x: number, y: number, width: number, height: number): Rect => ({
@@ -387,4 +405,20 @@ export const rect = (x: number, y: number, width: number, height: number): Rect 
   y,
   width,
   height
+})
+
+// -------------------------------------------------------------------------------------
+// instances
+// -------------------------------------------------------------------------------------
+
+/**
+ * The `Monoid` instance for a `Path`.
+ *
+ * @category instances
+ * @since 1.0.0
+ */
+export const monoidPath: M.Monoid<Path> = M.getStructMonoid({
+  _tag: { concat: () => 'Path', empty: 'Path' },
+  closed: M.monoidAny,
+  points: RA.getMonoid<Point>()
 })
